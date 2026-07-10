56327d1094a6e8d7178b3b61ec59eb93749864b2
Mesut Yakut <ce.mstykt@gmail.com>
2026-07-10 09:02:24 +0300

    updated atra to 1.2.0

--- Formula/atra.rb
+++ Formula/atra.rb
@@ -1,26 +1,26 @@
 class Atra < Formula
   desc "A fast, modern HTTP client CLI with load testing and flow runner"
   homepage "https://atrahasis.dev"
-  version "1.1.0"
+  version "1.2.0"
   license "Proprietary"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/atrahasisdev/cli-releases/releases/download/v1.1.0/atra-aarch64-apple-darwin.tar.gz"
-      sha256 "6a23814d5851556df64376e7e7fbd888107b35d1bd77ccb49d6c3638b418aaae"
+      url "https://github.com/atrahasisdev/cli-releases/releases/download/v1.2.0/atra-aarch64-apple-darwin.tar.gz"
+      sha256 "c613581733d536bd1007a927ac29b38a0f8d9ee49ea814786cd9e36a78606ea9"
     else
-      url "https://github.com/atrahasisdev/cli-releases/releases/download/v1.1.0/atra-x86_64-apple-darwin.tar.gz"
-      sha256 "543d28d1b6bd19c70c23c129bfef49b02fef78a310867a0348f43ac5c9e262fe"
+      url "https://github.com/atrahasisdev/cli-releases/releases/download/v1.2.0/atra-x86_64-apple-darwin.tar.gz"
+      sha256 "74883a16d8c2dbff2452d6ccd8d81cc44238a39ccd2c329b5b9c6d6e82040d6f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/atrahasisdev/cli-releases/releases/download/v1.1.0/atra-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "dc0ef5a8ec352fe666c9ca770089a6d3bbe773f0ccc5d8523a17d3ea3a32529b"
+      url "https://github.com/atrahasisdev/cli-releases/releases/download/v1.2.0/atra-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "c0ec26804c5cfcabf827aeac32e33412f8806545736d0f1e149b96b3258aae27"
     else
-      url "https://github.com/atrahasisdev/cli-releases/releases/download/v1.1.0/atra-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "e763402a304ab84e6391228292a481a33fa0e7224539f4321684a513a7f371d6"
+      url "https://github.com/atrahasisdev/cli-releases/releases/download/v1.2.0/atra-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "8223a851cfeef824e8b1de9adc07bb98482fc1c57b0742e15ebc8847302bae0a"
     end
   end
 
